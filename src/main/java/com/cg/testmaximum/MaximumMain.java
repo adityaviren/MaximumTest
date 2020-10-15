35dda794869e9c3df39ac10ac4d9c13ae6616245
Aditya Viren <adityaviren@gmail.com>
2020-10-15 20:18:31 +0530

    added printMax method to print max element

--- src/main/java/com/cg/testmaximum/MaximumMain.java
+++ src/main/java/com/cg/testmaximum/MaximumMain.java
@@ -15,10 +15,10 @@ public class MaximumMain<T extends Comparable> {
         Collections.sort(array_list);
         return array_list.get(array_list.size()-1);
     }
-    /*public void printMax(){
+    public void printMax(){
         List<T> array_list=new ArrayList<>();
         array_list=Arrays.asList(inputArray);
         Collections.sort(array_list);
         System.out.println(array_list.get(array_list.size()-1));
-    }*/
+    }
 }
--- src/test/java/com/cg/testmaximum/TestMaximum.java
+++ src/test/java/com/cg/testmaximum/TestMaximum.java
@@ -14,14 +14,14 @@ public class TestMaximum {
     public void maximumTest(){
         String[] arr={"Mark","Imran","Prince"};
         MaximumMain maximumMain=new MaximumMain(arr);
-        /*printMax(arr);*/
+        printMax(arr);
         Assert.assertEquals("Prince",maximumMain.maximum());
     }
 
-    /*public<T extends Comparable> void printMax(T[] inputArray){
+    public<T extends Comparable> void printMax(T[] inputArray){
         List<T> array_list=new ArrayList<>();
         array_list= Arrays.asList(inputArray);
         Collections.sort(array_list);
         System.out.println(array_list.get(array_list.size()-1));
-    }*/
+    }
 }
